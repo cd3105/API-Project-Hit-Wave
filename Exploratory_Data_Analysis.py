0d18bc69c2d2460831ec77400afcd6afc9acb07e
cdn243 <casperdaemen@gmail.com>
2024-12-19 16:55:26 +0100

    Final Addition

--- Exploratory_Data_Analysis.py
+++ Exploratory_Data_Analysis.py
@@ -4,6 +4,31 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("Final Datasets after Further Filtering\Reordered_Preprocessed_Labeled_Songs_per_Hot_100_with_Spotify_Features_and_Audio_Features_52896.csv")
 
+pd.DataFrame({'Split': ['1960s', 
+                        '1970s', 
+                        '1980s', 
+                        '1990s', 
+                        '2000s', 
+                        '2010s', 
+                        '2020s', 
+                        'Full'],
+              'Hits': [len(df[(df['Release Year'] >= 1960) & (df['Release Year'] < 1970) & (df['Hit'] == 1)]),
+                       len(df[(df['Release Year'] >= 1970) & (df['Release Year'] < 1980) & (df['Hit'] == 1)]),
+                       len(df[(df['Release Year'] >= 1980) & (df['Release Year'] < 1990) & (df['Hit'] == 1)]),
+                       len(df[(df['Release Year'] >= 1990) & (df['Release Year'] < 2000) & (df['Hit'] == 1)]),
+                       len(df[(df['Release Year'] >= 2000) & (df['Release Year'] < 2010) & (df['Hit'] == 1)]),
+                       len(df[(df['Release Year'] >= 2010) & (df['Release Year'] < 2020) & (df['Hit'] == 1)]),
+                       len(df[(df['Release Year'] >= 2020) & (df['Hit'] == 1)]),
+                       len(df[df['Hit'] == 1])],
+              'No Hits': [len(df[(df['Release Year'] >= 1960) & (df['Release Year'] < 1970) & (df['Hit'] == 0)]),
+                          len(df[(df['Release Year'] >= 1970) & (df['Release Year'] < 1980) & (df['Hit'] == 0)]),
+                          len(df[(df['Release Year'] >= 1980) & (df['Release Year'] < 1990) & (df['Hit'] == 0)]),
+                          len(df[(df['Release Year'] >= 1990) & (df['Release Year'] < 2000) & (df['Hit'] == 0)]),
+                          len(df[(df['Release Year'] >= 2000) & (df['Release Year'] < 2010) & (df['Hit'] == 0)]),
+                          len(df[(df['Release Year'] >= 2010) & (df['Release Year'] < 2020) & (df['Hit'] == 0)]),
+                          len(df[(df['Release Year'] >= 2020) & (df['Hit'] == 0)]),
+                          len(df[df['Hit'] == 0])]}).to_csv("EDA Datasets/Hits_n_No_Hits_per_Split.csv")
+
 sns.countplot(x='Hit', data=df, palette='Blues')
 plt.xticks([0, 1], ['No Hit', 'Hit'])
 plt.xlabel('Hit / No Hit')
